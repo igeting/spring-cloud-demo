1038bd83c28f42fc87a1dc2a06517837badd3f4c
c <imcling@qq.com>
2021-06-24 19:37:51 +0800

    eureka

--- eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class EurekaApplicationTests {
 
     @Test
     void contextLoads() {
